67f18a1ed020633a2b03e8edfe4a86ccc4e7f455
Demetri Moutis <demetrimoutis@gmail.com>
2018-10-02 08:33:14 -0400

    finished pset

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * INSTRUCTIONS.
  * 
@@ -25,22 +27,95 @@ public class ProblemSet2 {
 	}
 	
 	public void sayHello() {
-		
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		String firstname = in.nextLine();
+		String lastname = in.nextLine();
+		System.out.println("Hello, " + firstname + lastname);
 	}
 	
 	public void gradeMe() {
+		double homework = 0;
+		System.out.print("Enter your 1st homework grade:");
+		double homework = in.nextDouble();
+		System.out.print("Enter your 2nd homework grade:");
+		double homework = in.nextDouble();
+		System.out.print("Enter your 3rd homework grade:");
+		double homework = in.nextDouble();
+		homework /= 3;
 		
+		double quiz = 0;
+		System.out.print("Enter your 1st quiz grade:");
+		double quiz = in.nextDouble();
+		System.out.print("Enter your 2nd quiz grade:");
+		double quiz = in.nextDouble();
+		System.out.print("Enter your 3rd quiz grade:");
+		double quiz = in.nextDouble();
+		quiz /= 3;
+
+		double test = 0;
+		System.out.print("Enter your 1st test grade:");
+		test += in.nextDouble();
+		System.out.print("Enter your 2nd test grade:");
+		test += in.nextDouble();
+		System.out.print("Enter your 3rd test grade:");
+		test += in.nextDouble();
+		test /= 3;
+		
+		double final_grade = hmwk*0.15 + quiz*0.30 + test *0.55;
+		System.out.printf("%.2f%",final_grade);
+		in.close();	
 	}
 	
 	public void groupUs() {
-		
+		System.out.println("How many students are attending the trip?");
+		Scanner in = new Scanner(System.in);
+		int students = in.nextInt();
+		System.out.println("How many teachers are attending the trip?");
+		int teachers = in.nextInt();	
 	}
 	
 	public void info() {
-		
+		System.out.println("Enter your first and last name.");
+		Scanner in = new Scanner(System.in);
+		String fname = in.nextLine();
+		String lname = in.nextLine();
+		System.out.println("Enter your grade level.");
+		int grade = in.nextInt();
+		System.out.println("Enter your age.");
+		int age = in.nextInt();
+		String town = in.nextLine();
+		System.out.println("Enter your hometown.");
+		System.out.println("Name    : " + fname + " " + lname);
+		System.out.println("Grade    : " + grade);
+		System.out.println("Age    : " + age);
+		System.out.println("Hometown    : " + town);	
 	}
 	
 	public void initials() {
-		
+		Scanner in =new Scanner(System.in);
+        System.out.print("Enter your full name:" );
+        String name= in.nextLine();
+        int l = name.length();
+        int pos = 0;
+        for(int i=l-1;i>=0;i--)
+        {
+            char ch=name.charAt(i);
+            if(ch==' ')
+            {
+                pos=i;
+                break;
+            }
+        }
+        System.out.print("Your intials are: ");
+        System.out.print(name.charAt(0)+".");
+        for(int x=1;x<pos;x++)
+        {
+            char ch=name.charAt(x);
+                  if(ch==' ')
+            {
+                                
+                System.out.print(name.charAt(x+1)+".");
+            }	
 	}
-}
+        in.close();}}
